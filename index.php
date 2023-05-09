643f106212fbd842e9b7dbbcadd417cf3b7ab5a8
Aurélien Uguen <aurelien.uguen@gmail.com>
2023-05-09 10:25:19 +0200

    While Loop to increment number from 1 to 50

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<?php
+<?php 
 
-$animes = ["Jojo's Bizarre Adventure", "Death Note", "My Hero Academia", "Akira", "Dragon Ball"];
+$i = 1;
+
+while ($i <= 50) {
+    echo $i++;
+}
 
 ?>
-
-<table>
-    <thead>
-        <tr>
-            <th>Title</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($animes as $anime) { ?>
-        <tr>
-            <td><?php echo $anime?></td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
