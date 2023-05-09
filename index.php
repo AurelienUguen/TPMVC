f7e3c65e150bf720077c9fee325b44620e49c22b
Aurélien Uguen <aurelien.uguen@gmail.com>
2023-05-09 12:05:54 +0200

    GET METHOD

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
+<form action="" method="GET">
+    <label for="name">Name : </label>
+    <input type="text" name="name" id="name" placeholder="name">
+    <input type="submit" value="Envoyer">
+</form>
+
 <?php 
 
-$word = "coucou";
-echo ucfirst($word);
+if ($_GET) {
+    echo $_GET['name'];
+}
 
 ?>
