1efeb16c0facaeba62cec634b45ca86c9f5ab1c5
Aurélien Uguen <aurelien.uguen@gmail.com>
2023-05-09 11:15:24 +0200

    stringh lenght exercice

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php 
 
-$age = -5;
-
-
-if ($age >= 18) {
-    echo "Je suis majeur";
-} elseif ($age < 18 && $age > 0) {
-    echo "Je suis mineur";
-} else {
-    echo "Vous n'êtes pas humain";
-}
-
-switch ($age) {
-    case $age >= 18:
-        echo "Je suis majeur";
-    break;
-    case $age < 18 && $age > 0: 
-        echo "Je suis mineur";
-    break;
-    default:
-        echo "vous n'êtes pas humain";
-}
+$size = strlen("Ceci est une chaîne de caractère.");
+echo $size;
 
 ?>
