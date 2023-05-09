d271e858a3a148e518d7103b7fec896d94b3a895
Aurélien Uguen <aurelien.uguen@gmail.com>
2023-05-09 11:36:23 +0200

    correction exo 7

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php 
 
-$number1 = 37;
-$number2 = 14;
+$number1 = -3;
+$number2 = -4;
 
 if ($number1 > $number2) {
     echo $number1;
+} elseif ($number1 === $number2) {
+    echo "Les chiffres sont égaux";
 } else {
     echo $number2;
 }
+
+/* echo ($number1 > $number2) ? $number1 : $number2; */
 
 ?>
