c42bb6c9f15b2fef8dc7f2330471b6df8ee97ae7
Aurélien Uguen <aurelien.uguen@gmail.com>
2023-05-09 10:07:50 +0200

    Adding Anime table

--- index.php
+++ index.php
@@ -3,3 +3,28 @@
 echo "Hello World";
 
 ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Title</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Jojo's Bizzare Adventure</td>
+        </tr>
+        <tr>
+            <td>Death Note</td>
+        </tr>
+        <tr>
+            <td>My Hero Academdia</td>
+        </tr>
+        <tr>
+            <td>Akira</td>
+        </tr>
+        <tr>
+            <td>Dragon Ball</td>
+        </tr>
+    </tbody>
+</table>
